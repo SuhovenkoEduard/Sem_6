0d294d00664b589d45203b1c307b496e76c31d92
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2022-02-26 01:34:17 +0300

    RPI LAB2 (create folder if folder isn't exist)

--- RPI/Lab2/code/src/tasks/logs.ts
+++ RPI/Lab2/code/src/tasks/logs.ts
@@ -1,5 +1,9 @@
 import fs from 'fs'
+import { ERRORS_LOG_DIRPATH } from '../constants'
 
 export const logs = (filePath: string, options: object) => {
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(ERRORS_LOG_DIRPATH)
+  }
   fs.writeFileSync(filePath, JSON.stringify(options, null, '  '))
 }
