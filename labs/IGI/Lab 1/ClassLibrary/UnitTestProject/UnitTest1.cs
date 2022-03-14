4381d63946410509efc83544c0af3dff595a1b93
SuhovenkoEduard <avatar_dj@mail.ru>
2022-03-14 14:02:38 +0300

    Labs relaunch

--- labs/IGI/Lab 1/ClassLibrary/UnitTestProject/UnitTest1.cs
+++ labs/IGI/Lab 1/ClassLibrary/UnitTestProject/UnitTest1.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ClassLibrary.Entities;
+using ClassLibrary.Interfaces;
+using ClassLibrary.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace UnitTestProject
 {
@@ -9,6 +13,18 @@ namespace UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
+            Store store = new Store();
+            store.NameTovar = "Added";
+            store.Cost = 120;
+            store.Count = 23;
+            store.ClientId = 1;
+
+            IRepository<Store> storeRep = new RepositoryStore();
+            storeRep.Insert(store);
+            List<Store> searchedStores = storeRep.Search(store.NameTovar);
+            Store searchedStore = searchedStores.Find((cur) => cur.NameTovar == store.NameTovar);
+            Assert.IsNotNull(searchedStore);
+            storeRep.Delete(searchedStore.Id);
         }
     }
 }
