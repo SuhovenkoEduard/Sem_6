ffa6b934e2c0d5ab6f8f1f32b979f05cef7ad8d9
SuhovenkoEduard <avatar_dj@mail.ru>
2022-04-06 10:49:58 +0300

    RPI (Lab5) refactoring

--- labs/RPI/Lab5/code/Lab 5 Samovend/Lab 1/index.js
+++ labs/RPI/Lab5/code/Lab 5 Samovend/Lab 1/index.js
@@ -6,9 +6,9 @@ const router = require('./routers/index');
 app.use('/api', router);
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`)
-    } else {
-        console.log(`Server listening at port ${port}`);
-    }
+  if (err) {
+    console.log(`Error: ${err}`)
+  } else {
+    console.log(`Server listening at port ${port}`);
+  }
 });
