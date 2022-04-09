fb2f65b803bfb550f17553212504a3404f62ad18
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2022-04-09 17:21:41 +0300

    RPI Lab5 change folder name and delete old version

--- labs/RPI/Lab5/code/Lab 5 Samovend/Lab 2/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:', {
-  host: 'localhost', dialect: 'sqlite', pool: {
-    max: 5, min: 0, idle: 10000
-  }, storage: '../database.db'
-});
-
-module.exports = sequelize;
