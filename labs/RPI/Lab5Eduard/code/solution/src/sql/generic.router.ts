d1812fee260b8637121e184c1837a54aef1522e2
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2022-04-09 17:19:01 +0300

    RPI Lab5 (4/4 task), code in report is invalid

--- labs/RPI/Lab5Eduard/code/solution/src/sql/generic.router.ts
+++ labs/RPI/Lab5Eduard/code/solution/src/sql/generic.router.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import sqlite3 from 'sqlite3'
-import { PATH_TO_DATABASE, Routes } from '../constants/constants'
+import { ApiRoutes, PATH_TO_DATABASE } from '../constants/constants'
 import {
   createDeleteQuery,
   createGetAllQuery,
   createGetByIdQuery,
   createPostQuery,
   createPutQuery,
-} from '../helpers/dbQueries.helper'
+} from '../helpers/dbQueries.helpers'
 import { ResponseStatuses } from '../constants/types'
 import { messages } from '../constants/messages'
 
@@ -25,27 +25,27 @@ export const createRouter = (tableName: string, propNames: string[]) => {
 
   const [propNameId] = propNames
 
-  router.get(Routes.getAll, (req: express.Request, res: express.Response) => {
+  router.get(ApiRoutes.getAll, (req: express.Request, res: express.Response) => {
     db.all(createGetAllQuery(tableName), requestHandler(res))
   })
 
-  router.get(Routes.getSingle, (req: express.Request, res: express.Response) => {
+  router.get(ApiRoutes.getSingle, (req: express.Request, res: express.Response) => {
     const id = +req.params.id
     db.all(createGetByIdQuery(tableName, propNameId), [id], requestHandler(res))
   })
 
-  router.put(Routes.putSingle, (req: express.Request, res: express.Response) => {
+  router.put(ApiRoutes.putSingle, (req: express.Request, res: express.Response) => {
     const id = +req.params.id
     db.all(createPutQuery(tableName, Object.keys(req.body), propNameId), [...Object.values(req.body), id],
       requestHandler(res, messages.SUCCESSFULLY_UPDATED))
   })
 
-  router.post(Routes.postSingle, (req: express.Request, res: express.Response) => {
+  router.post(ApiRoutes.postSingle, (req: express.Request, res: express.Response) => {
     db.all(createPostQuery(tableName, Object.keys(req.body)), Object.values(req.body),
       requestHandler(res, messages.SUCCESSFULLY_ADDED))
   })
 
-  router.delete(Routes.deleteSingle, (req: express.Request, res: express.Response) => {
+  router.delete(ApiRoutes.deleteSingle, (req: express.Request, res: express.Response) => {
     const id = +req.params.id
     db.all(createDeleteQuery(tableName, propNameId), [id],
       requestHandler(res, messages.SUCCESSFULLY_DELETED))
